368c39f11e8f12ea358a46314ab3963bec394f62
Karim Ben Romdhane <karoumbr@gmail.com>
2023-08-02 09:52:38 +0100

    second commit

--- src/app/tableau/tableau.component.ts
+++ src/app/tableau/tableau.component.ts
@@ -7,19 +7,20 @@ import { Component } from '@angular/core';
 })
 export class TableauComponent {
 
-  style:string="italic";
-  titre:string="1. Opérations sur un tableau";
-titre2:string="2. Affichage des boutons des éléments pairs du tableau";
+  style: string = "italic";
+  titre: string = "1. Opérations sur un tableau";
+  titre2: string = "2. Affichage des boutons des éléments pairs du tableau";
 
-  tab= [15, 46, 0, 89, 80];
-  onPosition(ind:number,cpt:number){
-    console.log(this.tab[ind] + " a la position " + (ind+1)  + "/" + cpt );
+  tab = [15, 46, 0, 89, 80];
+  onPosition(ind: number, cpt: number) {
+    console.log(this.tab[ind] + " a la position " + (ind + 1) + "/" + cpt);
+    console.log(this.tab[ind]);
   }
-  colorier(nb:number){
-    if(nb%3==0){
+  colorier(nb: number) {
+    if (nb % 3 == 0) {
       return "magenta";
     }
-    else{
+    else {
       return "blue";
     }
   }
